89170b8f58c5021461870ce059b5d66ce5db2032
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2018-04-29 19:45:06 +0300

    first release

---
+++ index.js
@@ -0,0 +1,38 @@
+import dot from 'dot-prop';
+
+export function mapStateTwoWay(targetPath, action, props) {
+  let result = {};
+  let target = null;
+  ((typeof props === 'string') ? [props] : props).forEach(function (prop) {
+    result[prop] = {
+      get() {
+        if (target === null) {
+          target = dot.get(this.$store.state, targetPath);
+        }
+        return target[prop];
+      },
+      set(value) {
+        this.$store.dispatch(action, (typeof props === 'string') ? value : { [prop]: value });
+      }
+    };
+  });
+  return result;
+}
+export function mapArrayItemTwoWay(targetItem, action, props) {
+  let result = {};
+  props.forEach(function (prop) {
+    result[prop] = {
+      get() {
+        return this[targetItem][prop];
+      },
+      set(value) {
+        this.$store.dispatch(action, {
+          item: this[targetItem],
+          field: prop,
+          value
+        });
+      }
+    };
+  });
+  return result;
+}
